7768f00682412f9eaa6ada3480410b10e4b5a5da
Michel Banagouro <michel@leanwork.com.br>
2023-05-31 19:32:52 -0300

    Singleton

---
+++ CursoPOO/CursoPOO/DesignPatterns/Criacional/Configuracao.cs
@@ -0,0 +1,19 @@
+﻿namespace CursoPOO.DesignPatterns.Criacional;
+
+// **********************************************************************
+// Singleton https://refactoring.guru/pt-br/design-patterns/singleton
+// **********************************************************************
+public class Configuracao
+{
+    private static Configuracao _config;
+
+    private Configuracao() { }
+
+    public static Configuracao Instancia
+    {
+        get { return _config = (_config ?? new Configuracao()); }
+    }
+
+    //public static Configuracao Instancia 
+    //    => _config = (_config ?? new Configuracao());
+}
